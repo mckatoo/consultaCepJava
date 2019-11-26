9f6e8f08f663386c631ab18a76ac51af1e4eacce
Milton Carlos Katoo <mckatoo@gmail.com>
2019-11-26 18:06:36 -0300

    FIRST COMMIT

---
+++ src/cep/correios/Correios.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cep.correios;
+
+import cep.EnderecoERP;
+import cep.SQLException_Exception;
+import cep.SigepClienteException;
+
+/**
+ *
+ * @author mckatoo
+ */
+public class Correios {
+
+    private static EnderecoERP consultaCEP(java.lang.String cep) throws SigepClienteException, SQLException_Exception {
+        cep.AtendeClienteService service = new cep.AtendeClienteService();
+        cep.AtendeCliente port = service.getAtendeClientePort();
+        return port.consultaCEP(cep);
+    }
+
+}
